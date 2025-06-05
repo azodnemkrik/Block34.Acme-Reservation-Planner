5ce50a84d82100f5c1331dfb86f400aba9b117bd
Kirk Mendoza <kirkmendoza@gmail.com>
2025-06-05 01:31:56 -0500

    Successfull created CUSTOMERS, RESTAURANTS and RESERVATIONS tables

--- db.js
+++ db.js
@@ -16,3 +16,35 @@ const fetchRestaurants = () => {}
 
 // DELETE
 const destroyReservation = () => {}
+
+const seed = async () => {
+	const SQL = `
+		DROP TABLE IF EXISTS reservations;
+		DROP TABLE IF EXISTS customers;
+		DROP TABLE IF EXISTS restaurants;
+
+		CREATE TABLE customers(
+			id UUID PRIMARY KEY,
+			name VARCHAR(100)
+		);
+
+		CREATE TABLE restaurants(
+			id UUID PRIMARY KEY,
+			name VARCHAR(100)
+		);
+		
+		CREATE TABLE reservations(
+			id UUID PRIMARY KEY,
+			date TIMESTAMP DEFAULT now(),
+			party_count INTEGER DEFAULT 1,
+			restaurant_id UUID REFERENCES restaurants(id) NOT NULL,
+			customer_id UUID REFERENCES customers(id)
+		);
+	`
+	await client.query(SQL)
+}
+
+module.exports = {
+	seed,
+	client,
+}
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
+const { seed , client } = require('./db.js')
 
 app.use(express.json())
 
-const init = () => {
+const init = async () => {
+	await client.connect()
+	await seed()
+
 	const PORT = 3000
 	app.listen(PORT, () => {
 		console.log(`Listening to PORT: ${PORT}`)

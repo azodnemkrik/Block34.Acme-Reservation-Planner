7545542b4594c16b1ef1e4fda91013d75e1e0bfa
Kirk Mendoza <kirkmendoza@gmail.com>
2025-06-05 01:01:17 -0500

    Added comments for structure and comprehension

---
+++ api.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express.Router()
+
+// CREATE
+app.post()
+
+// READ
+app.get()
+
+// UPDATE
+app.put()
+
+// DELETE
+app.delete()
--- db.js
+++ db.js
@@ -1,2 +1,9 @@
 const pg = require('pg')
-const client = new pg.Client('postgres://localhost/acme_reservation_planner')
+const client = new pg.Client('postgres://localhost/acme_reservation_planner')   
+const {v4} = require('uuid')
+const uuidv4 = v4
+
+// CREATE
+// READ
+// UPDATE
+// DELETE

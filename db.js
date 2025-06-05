995f5cab95e5ec984c35a5d2fd62088500c6b60a
Kirk Mendoza <kirkmendoza@gmail.com>
2025-06-05 00:56:49 -0500

    Added JS files for structure

---
+++ db.js
@@ -0,0 +1,2 @@
+const pg = require('pg')
+const client = new pg.Client('postgres://localhost/acme_reservation_planner')
---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+const init = () => {
+    const PORT = 3000
+    app.listen(PORT, () => {
+        console.log(`Listening to PORT: ${PORT}`)
+    })
+}
+
+init()
